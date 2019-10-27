308ae1da987f03d203fcefe6239583f77583bcdc
gintonick <rezu@mail.ru>
2019-10-27 18:56:27 +0200

    # customer

---
+++ src/org/btarikool/javacourse/Allergie.java
@@ -0,0 +1,6 @@
+package org.btarikool.javacourse;
+
+public interface Allergie {
+
+}
+
---
+++ src/org/btarikool/javacourse/Customer.java
@@ -0,0 +1,19 @@
+package org.btarikool.javacourse;
+
+public class Customer {
+   class budget {
+       int budget = 5000;
+       double summa;
+
+   }
+
+   public interface CurrencyConverter {
+        float convert(String currencyFrom, String currencyTo)
+                throws Exception;
+    }
+   public static void allergie() {
+         String dogAllergy;
+         String catAllergy;
+         String featherAllergy;
+    }
+}
